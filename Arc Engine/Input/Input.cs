bb37f8fda6c16041fe10f00d073924fb4c11dc03
AshkoreDracson <AshkoreD@gmail.com>
2017-11-19 02:43:52 +0100

    Added first Input code

---
+++ Arc Engine/Input/Input.cs
@@ -0,0 +1,18 @@
+﻿namespace ArcEngine
+{
+    public static class Input
+    {
+        public static bool IsKeyDown(Keys key)
+        {
+            return false;
+        }
+        public static bool IsKey(Keys key)
+        {
+            return false;
+        }
+        public static bool IsKeyUp(Keys key)
+        {
+            return false;
+        }
+    }
+}
---
+++ Arc Engine/Input/KeyState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ArcEngine
+{
+    [Flags]
+    public enum KeyState : byte
+    {
+        None = 0x00,
+        Pressed = 0x01,
+        Held = 0x02,
+        Released = 0x04
+    }
+}
