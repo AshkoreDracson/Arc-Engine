de11695bc87af5891f60f09325f316d7992aabd7
AshkoreDracson <AshkoreD@gmail.com>
2017-11-23 22:56:36 +0100

    Modified TestScript

--- Arc Engine Test/TestScript.cs
+++ Arc Engine Test/TestScript.cs
@@ -4,19 +4,25 @@ namespace ArcEngineTest
 {
     public class TestScript : GlobalScript
     {
-        private TextRenderer tr;
+        private TextRenderer[] tr = new TextRenderer[50];
 
         public override void Start()
         {
-            GameObject go = new GameObject();
-            tr = go.AddComponent<TextRenderer>();
-            tr.Text = "This is a test";
-            tr.Position = new Vector2(10, 10);
+            for (int i = 0; i < tr.Length; i++)
+            {
+                GameObject go = new GameObject();
+                tr[i] = go.AddComponent<TextRenderer>();
+                tr[i].Text = i.ToString();
+                tr[i].Position = new Vector2(10 + i * 10, 10 + i * 10);
+            }
         }
 
         public override void Update()
         {
-            tr.Position = new Vector2(10, 10 + (Time.time * 6).Cos().Remap(-1, 1, 50, 0));
+            for (int i = 0; i < tr.Length; i++)
+            {
+                tr[i].Position = new Vector2(10 + i * 10, 10 + (Time.time * 6 + (i)).Cos().Remap(-1, 1, 50, 0) + i * 10);
+            }
         }
     }
 }
